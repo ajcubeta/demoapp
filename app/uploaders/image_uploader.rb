4a759f7d5a7eb32d845dfc06e5552b9411e5f594
Andre Joseph Cubeta <ajcubeta@gmail.com>
2011-11-02 17:33:21 +0800

    Test files storage uploading images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,18 +3,26 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or ImageScience support:
-  # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
-  storage :fog
+  storage :file
+  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+  # def store_dir
+  #   "/"
+  # end
+
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+    # "#{Rails.root}/private/assets/avatars"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -36,9 +44,9 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
